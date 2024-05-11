8c86446c58c14981b0e57a4ed7d94def78bfc056
Pedro Moura <pedrosousavn@gmail.com>
2024-05-11 12:57:47 -0300

    criando if para validar user futuramente no back

--- source/script/verifyLogin.js
+++ source/script/verifyLogin.js
@@ -12,18 +12,42 @@ function redirecionarPara(url) {
 const idUsuario = obterParametroGET('id');
 
 if (idUsuario !== null) {
-    // Tente obter os dados do localStorage
     const projectsData = localStorage.getItem('projectsData');
 
-    
     if (projectsData) {
-
         window.projects = JSON.parse(projectsData);
-
     } else {
         console.error('Erro: Dados não encontrados na localStorage');
-        redirecionarPara(`/api/?id=${idUsuario}`);
+        carregarDados('/api/?id=' + idUsuario);
     }
 } else {
     redirecionarPara('/login');
+}
+
+function carregarDados(url) {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', url, true);
+
+    xhr.onload = function() {
+        if (xhr.status >= 200 && xhr.status < 300) {
+
+            const data = JSON.parse(xhr.responseText);
+
+            localStorage.setItem('projectsData', JSON.stringify(data));
+        } else {
+            console.error('Erro ao carregar os dados: ' + xhr.statusText);
+        }
+    };
+
+    xhr.onerror = function() {
+        console.error('Erro de rede ao tentar carregar os dados');
+    };
+
+    xhr.send();
+}
+
+
+if (idUsuario !== 'pedromoura') {
+    redirecionarPara(`../login/`);
 }
